dfca8ef2751002ae55c638891f320c69c33d20a3
Juan Pablo Restrepo <juanresco@unisabana.edu.co>
2024-03-08 18:23:33 -0500

    A VER

--- scripts/js.js
+++ scripts/js.js
@@ -1,9 +1,7 @@
-const button = document.querySelector('#fetchbutton');
-const quoteElement = document.getElementById('quote');
-const imageElement = document.getElementById('image');
-
-button.addEventListener('click', async () => {
+// Espera a que la ventana esté completamente cargada
+window.onload = async function() {
     try {
+        // Realizar la solicitud fetch para obtener la cita y la imagen
         const data = await fetch('https://thesimpsonsquoteapi.glitch.me/quotes');
         const comments = await data.json();
 
@@ -11,13 +9,16 @@ button.addEventListener('click', async () => {
             const quote = comments[0].quote;
             const imageUrl = comments[0].image;
 
+            // Actualizar los elementos HTML con la cita y la imagen obtenidas
+            const quoteElement = document.getElementById('quote');
             quoteElement.textContent = quote;
+
+            const imageElement = document.getElementById('image');
             imageElement.src = imageUrl;
         } else {
-            quoteElement.textContent = "No se encontraron citas.";
+            console.error('No se encontraron citas.');
         }
     } catch (error) {
         console.error('Error fetching quote:', error);
-        quoteElement.textContent = "Error al cargar la cita.";
     }
-});
+};
