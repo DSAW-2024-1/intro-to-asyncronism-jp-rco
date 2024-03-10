3c64336ce7352eb0f7b9dfaa25cbeb96af33238d
Juan Pablo Restrepo <juanresco@unisabana.edu.co>
2024-03-10 01:35:24 -0500

    NO SE QUE HICE ACA EN ESTE CAMBIO PERO ES LO ULTIMO QUE HE HECHO

--- scripts/js.js
+++ scripts/js.js
@@ -3,28 +3,25 @@ const quoteElement = document.getElementById('quote');
 const imageElement = document.getElementById('image');
 const sendButton = document.querySelector('.send');
 const inputElement = document.getElementById('input');
+const inputFilterElement = document.getElementById('input-f');
+const filtroButton = document.getElementById('filtrob');
 
-// Variable para almacenar el nombre del personaje actualmente mostrado en la imagen
 let currentCharacter = '';
 
-// Espera a que la ventana esté completamente cargada
 window.onload = async function() {
     try {
-        // Realizar la solicitud fetch para obtener la cita y la imagen
         const data = await fetch('https://thesimpsonsquoteapi.glitch.me/quotes');
         const comments = await data.json();
 
         if (comments.length > 0) {
             const quote = comments[0].quote;
             const imageUrl = comments[0].image;
-            const characterName = comments[0].character; // Obtener el nombre del personaje actualmente mostrado
-
-            // Actualizar los elementos HTML con la cita, la imagen y el nombre del personaje obtenidos
+            const characterName = comments[0].character; 
             quoteElement.textContent = quote;
             imageElement.src = imageUrl;
-            currentCharacter = characterName; // Guardar el nombre del personaje
+            currentCharacter = characterName; 
             const nameElement = document.getElementById('name');
-            nameElement.textContent = characterName; // Actualizar el elemento HTML con el nombre del personaje
+            nameElement.textContent = characterName; 
         } else {
             console.error('No se encontraron citas.');
         }
@@ -41,13 +38,13 @@ button.addEventListener('click', async () => {
         if (comments.length > 0) {
             const quote = comments[0].quote;
             const imageUrl = comments[0].image;
-            const characterName = comments[0].character; // Obtener el nombre del nuevo personaje
+            const characterName = comments[0].character; 
 
             quoteElement.textContent = quote;
             imageElement.src = imageUrl;
-            currentCharacter = characterName; // Guardar el nombre del nuevo personaje
+            currentCharacter = characterName; 
             const nameElement = document.getElementById('name');
-            nameElement.textContent = characterName; // Actualizar el elemento HTML con el nombre del personaje
+            nameElement.textContent = characterName; 
         } else {
             quoteElement.textContent = "No se encontraron citas.";
         }
@@ -59,22 +56,46 @@ button.addEventListener('click', async () => {
 
 sendButton.addEventListener('click', async () => {
     try {
-        // Obtener el número ingresado por el usuario
         const numFrases = parseInt(inputElement.value);
 
-        // Realizar la solicitud fetch para obtener las frases del personaje actual
         const response = await fetch(`https://thesimpsonsquoteapi.glitch.me/quotes?count=${numFrases}&character=${currentCharacter}`);
         const data = await response.json();
 
-        // Construir una cadena que contenga todos los comentarios con números secuenciales
         let commentsText = '';
         data.forEach((item, index) => {
             const numSecuencial = index + 1;
-            commentsText += `<p>${numSecuencial}. ${item.quote}</p>`; // Agregar etiquetas <p> alrededor de cada frase
+            commentsText += `<p>${numSecuencial}. ${item.quote}</p>`; 
         });
 
-        // Mostrar los comentarios en el elemento <p>
-        quoteElement.innerHTML = commentsText; // Usar innerHTML para renderizar las etiquetas HTML
+        quoteElement.innerHTML = commentsText; 
+    } catch (error) {
+        console.error('Error fetching data:', error);
+        quoteElement.textContent = "Error al cargar los datos.";
+    }
+});
+
+filtroButton.addEventListener('click', async () => {
+    try {
+        const characterName = inputFilterElement.value;
+
+        const response = await fetch(`https://thesimpsonsquoteapi.glitch.me/quotes?character=${characterName}`);
+        const data = await response.json();
+
+        if (data.length > 0) {
+            const quote = data[0].quote;
+            const imageUrl = data[0].image;
+
+            quoteElement.textContent = quote;
+            imageElement.src = imageUrl;
+            currentCharacter = characterName; 
+            const nameElement = document.getElementById('name');
+            nameElement.textContent = characterName; 
+        } else {
+            quoteElement.textContent = `No se encontraron frases para ${characterName}.`;
+            imageElement.src = '';
+            const nameElement = document.getElementById('name');
+            nameElement.textContent = ''; 
+        }
     } catch (error) {
         console.error('Error fetching data:', error);
         quoteElement.textContent = "Error al cargar los datos.";
